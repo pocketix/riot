0383e6429a118c2a936b78acfed07977bbd8fc0d
Michal Bureš <MichalBRS2020@gmail.com>
2023-10-21 17:07:53 +0200

    The front-end and back-end now communicate through REST. Configuration adjustments: TypeScript (support for more modern language features...) and Fiber (CORS).

--- go-backend/src/main.go
+++ go-backend/src/main.go
@@ -1,13 +1,27 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func main() {
+
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:1234",
+	}))
 
 	app.Get("/run-demo", func(c *fiber.Ctx) error {
 		go runDemo()
 		return c.SendString("One should be able to run the DEMO in asynchronous manner by accessing this REST endpoint...")
+	})
+
+	app.Get("/fetch-data", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "Hello from Go back-end!",
+		})
 	})
 
 	err := app.Listen("localhost:8080")
--- parcel-react-frontend/src/ApplicationEntrypoint.tsx
+++ parcel-react-frontend/src/ApplicationEntrypoint.tsx
@@ -1,10 +1,28 @@
-import React, { useState } from "react"
+import React, {useEffect, useState} from "react"
+
+const fetchDataFromTheGoBackend = async (): Promise<any> => {
+
+    try {
+        const response: Response = await fetch("http://localhost:8080/fetch-data")
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+        return await response.json()
+    } catch (error) {
+        console.error("There was a problem with the data fetching operation: ", error.message);
+        throw error;
+    }
+}
 
 const ApplicationEntrypoint: React.FC = () => {
 
     const [count, setCount] = useState<number>(1)
 
     const onClickHandler = () => setCount(currentCount => currentCount + 1)
+
+    useEffect(() => {
+        fetchDataFromTheGoBackend().then(data => console.log("Data from backend: ", data))
+    }, []);
 
     return (
         <>
