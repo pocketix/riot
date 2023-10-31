3b2484d355ceeaea9b335616f6474cc4c4bd289f
Michal Bureš <MichalBRS2020@gmail.com>
2023-10-31 19:18:29 +0100

    Re-locating the generated.go file into a separate directory (package).

--- go-backend/src/api/graphql/server.go
+++ go-backend/src/api/graphql/server.go
@@ -1,6 +1,7 @@
 package graphql
 
 import (
+	"bp-bures-SfPDfSD/src/api/graphql/generated"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/adaptor"
@@ -8,7 +9,7 @@ import (
 
 func SetupGraphQLServer(app *fiber.App) {
 
-	graphQLServer := handler.NewDefaultServer(NewExecutableSchema(Config{Resolvers: &Resolver{}}))
+	graphQLServer := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &Resolver{}}))
 
 	app.Use("/", adaptor.HTTPHandler(graphQLServer))
 }
