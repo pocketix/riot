60091f6e30329377ca8b9c69f0b41ad6c251f626
Michal Bureš <MichalBRS2020@gmail.com>
2023-10-31 11:44:22 +0100

    Introducing gqlgen through a new Go module...

--- go-backend/src/api/graphql/server.go
+++ go-backend/src/api/graphql/server.go
@@ -1,62 +1,28 @@
-package graphql
+package main
 
 import (
-	"github.com/gofiber/fiber/v2"
-	"github.com/graphql-go/graphql"
+	"bp-bures-SfPDfSD-graphQLAPI/graph"
 	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 )
 
-type input struct {
-	Query         string                 `query:"query"`
-	OperationName string                 `query:"operationName"`
-	Variables     map[string]interface{} `query:"variables"`
-}
+const defaultPort = "8080"
 
-func SetupGraphQLServer(app *fiber.App) {
-
-	schema, err := SetupGraphQLSchema()
-	if err != nil {
-		log.Println("Failed to setup a GraphQL schema!")
-		return
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
 	}
 
-	app.Get("/", func(ctx *fiber.Ctx) error {
-		var i input
-		if err := ctx.QueryParser(&i); err != nil {
-			return ctx.
-				Status(fiber.StatusInternalServerError).
-				SendString("Cannot parse query parameters: " + err.Error())
-		}
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
-		result := graphql.Do(graphql.Params{
-			Schema:         schema,
-			RequestString:  i.Query,
-			OperationName:  i.OperationName,
-			VariableValues: i.Variables,
-		})
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
 
-		ctx.Set("Content-Type", "application/graphql-response+json")
-		return ctx.JSON(result)
-	})
-
-	app.Post("/", func(ctx *fiber.Ctx) error {
-		var i input
-		if err := ctx.BodyParser(&i); err != nil {
-			return ctx.
-				Status(fiber.StatusInternalServerError).
-				SendString("Cannot parse body: " + err.Error())
-		}
-
-		result := graphql.Do(graphql.Params{
-			Schema:         schema,
-			RequestString:  i.Query,
-			OperationName:  i.OperationName,
-			VariableValues: i.Variables,
-		})
-
-		ctx.Set("Content-Type", "application/graphql-response+json")
-		return ctx.JSON(result)
-	})
-
-	log.Println("Finishing GraphQL server setup...")
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
