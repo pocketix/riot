aebe7dcb737698886c1c3c3f4b76d75dc3d0113a
Michal Bureš <MichalBRS2020@gmail.com>
2023-11-11 13:09:57 +0100

    FE: "SideNavigation" components merged and renamed into "MainLayout".

--- parcel-react-frontend/src/ApplicationEntrypoint.tsx
+++ parcel-react-frontend/src/ApplicationEntrypoint.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import {Routes, Route} from "react-router-dom"
-import SideNavigation from "./components/side-navigation/SideNavigation"
+import MainLayout from "./components/main-layout/MainLayout"
 import Homepage from "./pages/homepage/Homepage"
 import ApolloSandboxPage from "./pages/apollo-sandbox-page/ApolloSandboxPage"
 import FallbackPage from "./pages/fallback-page/FallbackPage"
@@ -9,7 +9,7 @@ const ApplicationEntrypoint: React.FC = () => {
 
     return <div>
         <Routes>
-            <Route path="/" element={<SideNavigation/>}>
+            <Route path="/" element={<MainLayout/>}>
                 <Route index element={<Homepage/>}/>
                 <Route path="apollo-sandbox" element={<ApolloSandboxPage/>}/>
                 <Route path="*" element={<FallbackPage/>}/>
---
+++ parcel-react-frontend/src/components/main-layout/MainLayout.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+import {Link, Outlet} from "react-router-dom"
+import styles from "./MainLayout.module.scss"
+
+const MainLayout: React.FC = () => {
+
+    return <div className={styles.outerContainer}>
+        <div className={styles.sidePanel}>
+            <CustomLink route="/" text="Homepage" />
+            <CustomLink route="/apollo-sandbox" text="Apollo Sandbox Page" />
+        </div>
+        <div className={styles.outletContainer}>
+            <Outlet />
+        </div>
+    </div>
+}
+
+export default MainLayout
+
+interface CustomLinkProps {
+    route: string
+    text: string
+}
+
+const CustomLink: React.FC<CustomLinkProps> = ({route, text}) => {
+
+    return <div className={styles.customLink}>
+        <Link to={route}>{text}</Link>
+    </div>
+}
