ff21761da43d293c8256913e3ed4b4f636a9d7d3
Michal Bureš <MichalBRS2020@gmail.com>
2023-11-13 00:35:57 +0100

    FE: Introducing arbitrary amount parameter specification to NewDeviceTypeForm.

--- parcel-react-frontend/src/pages/device-types-page/components/new-device-type-form/NewDeviceTypeForm.tsx
+++ parcel-react-frontend/src/pages/device-types-page/components/new-device-type-form/NewDeviceTypeForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useMemo, ChangeEvent } from 'react'
-import { Button, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField } from '@mui/material'
+import { Button, FormControl, Grid, InputLabel, MenuItem, Select, SelectChangeEvent, TextField } from '@mui/material'
 import styles from './NewDeviceTypeForm.module.scss'
 
 interface NewDeviceTypeFormProps {
@@ -10,50 +10,80 @@ interface NewDeviceTypeFormProps {
 
 const NewDeviceTypeForm: React.FC<NewDeviceTypeFormProps> = (props) => {
   const [denotation, setDenotation] = useState<string>('shelly1pro')
-  const [parameterName, setParameterName] = useState<string>('relay_0_temperature')
-  const [parameterType, setParameterType] = useState<string>('STRING')
+  const [parameters, setParameters] = useState<{ name: string; type: string }[]>([{ name: 'relay_0_temperature', type: 'NUMBER' }])
+
   const isFormDisabled: boolean = useMemo<boolean>(() => props.anyLoadingOccurs || props.anyErrorOccurred, [props.anyLoadingOccurs, props.anyErrorOccurred])
 
   const onSubmitHandler = useCallback(async () => {
-    if (denotation.length === 0) {
-      return
-    }
-    await props.createNewUserDefinedDeviceType(denotation, [
-      {
-        name: parameterName,
-        type: parameterType as 'STRING' | 'NUMBER' | 'BOOLEAN'
-      }
-    ])
-  }, [denotation, parameterName, parameterType, props.createNewUserDefinedDeviceType])
+    await props.createNewUserDefinedDeviceType(denotation, parameters as { name: string; type: 'STRING' | 'NUMBER' | 'BOOLEAN' }[])
+  }, [denotation, parameters, props.createNewUserDefinedDeviceType])
 
   const onDenotationChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setDenotation(e.target.value)
   }, [])
 
-  const onParameterNameChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setParameterName(e.target.value)
-  }, [])
+  const onParameterNameChange = (index: number) => (e: ChangeEvent<HTMLInputElement>) => {
+    const newParameters = [...parameters]
+    newParameters[index].name = e.target.value
+    setParameters(newParameters)
+  }
 
-  const onParameterTypeChange = useCallback((e: SelectChangeEvent) => {
-    setParameterType(e.target.value)
-  }, [])
+  const onParameterTypeChange = (index: number) => (e: SelectChangeEvent) => {
+    const newParameters = [...parameters]
+    newParameters[index].type = e.target.value
+    setParameters(newParameters)
+  }
+
+  const addParameter = () => {
+    setParameters([...parameters, { name: 'relay_0_temperature', type: 'NUMBER' }])
+  }
+
+  const deleteParameter = (index: number) => {
+    const newParameters = [...parameters]
+    newParameters.splice(index, 1)
+    setParameters(newParameters)
+  }
 
   return (
     <div className={styles.form}>
-      <h2>Define new device type</h2>
-      <TextField error={denotation.length === 0} label="Denotation" value={denotation} disabled={isFormDisabled} onChange={onDenotationChange} />
-      <TextField error={parameterName.length === 0} label="Parameter name" value={parameterName} disabled={isFormDisabled} onChange={onParameterNameChange} />
-      <FormControl>
-        <InputLabel id="parameter-type-select-label">Parameter type</InputLabel>
-        <Select id="parameter-type-select" labelId="parameter-type-select-label" label="Parameter type" value={parameterType} onChange={onParameterTypeChange} disabled={isFormDisabled}>
-          <MenuItem value={'STRING'}>STRING</MenuItem>
-          <MenuItem value={'NUMBER'}>NUMBER</MenuItem>
-          <MenuItem value={'BOOLEAN'}>BOOLEAN</MenuItem>
-        </Select>
-      </FormControl>
-      <Button disabled={isFormDisabled || denotation.length === 0 || parameterName.length === 0} onClick={onSubmitHandler}>
-        Submit
-      </Button>
+      <h2>Define a new device type</h2>
+      <Grid container spacing={2} alignItems="center">
+        <Grid item xs={12}>
+          <TextField fullWidth error={denotation.length === 0} label="Denotation" value={denotation} disabled={isFormDisabled} onChange={onDenotationChange} />
+        </Grid>
+        {parameters.map((parameter, index) => (
+          <React.Fragment key={index}>
+            <Grid item xs={4}>
+              <TextField fullWidth error={parameter.name.length === 0} label={`Parameter ${index + 1} – Name`} value={parameter.name} disabled={isFormDisabled} onChange={onParameterNameChange(index)} />
+            </Grid>
+            <Grid item xs={4}>
+              <FormControl fullWidth>
+                <InputLabel id={`parameter-type-select-label-${index}`}>{`Parameter ${index + 1} – Type`}</InputLabel>
+                <Select labelId={`parameter-type-select-label-${index}`} label={`Parameter ${index + 1} – Type`} value={parameter.type} onChange={onParameterTypeChange(index)} disabled={isFormDisabled}>
+                  <MenuItem value={'STRING'}>STRING</MenuItem>
+                  <MenuItem value={'NUMBER'}>NUMBER</MenuItem>
+                  <MenuItem value={'BOOLEAN'}>BOOLEAN</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={4}>
+              <Button disabled={isFormDisabled} onClick={() => deleteParameter(index)}>
+                Delete this parameter
+              </Button>
+            </Grid>
+          </React.Fragment>
+        ))}
+        <Grid item xs={12}>
+          <Button disabled={isFormDisabled} onClick={addParameter}>
+            Introduce next parameter
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Button fullWidth disabled={isFormDisabled || denotation.length === 0 || parameters.some((p) => p.name.length === 0)} onClick={onSubmitHandler}>
+            Submit
+          </Button>
+        </Grid>
+      </Grid>
     </div>
   )
 }
