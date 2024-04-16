747cc4b057cb8d17893eac294d25fa2641fd091a
Michal Bureš <MichalBRS2020@gmail.com>
2024-04-16 16:06:26 +0200

    Modal dialog base...

---
+++ frontend/src/page-independent-components/mui-modal-base/MuiModalBase.tsx
@@ -0,0 +1,40 @@
+import React, { ReactNode } from 'react'
+import { Box, Modal } from '@mui/material'
+import styles from './styles.module.scss'
+
+interface MuiModalBaseProps {
+  isOpen: boolean
+  onCloseHandler: () => void
+  modalTitle: string
+  content: ReactNode
+}
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  backgroundColor: '#ffffff',
+  border: '3px solid #000',
+  p: 2
+}
+
+const MuiModalBase: React.FC<MuiModalBaseProps> = (props) => {
+  return (
+    <div>
+      <Modal open={props.isOpen}>
+        <Box sx={style}>
+          <div className={styles.headerRow}>
+            <p>{props.modalTitle}</p>
+            <span onClick={props.onCloseHandler} className="material-symbols-outlined">
+              close
+            </span>
+          </div>
+          {props.content}
+        </Box>
+      </Modal>
+    </div>
+  )
+}
+
+export default MuiModalBase
--- frontend/src/pages/kpi-detail-page/components/change-logical-operation-type-modal/ChangeLogicalOperationTypeModal.tsx
+++ frontend/src/pages/kpi-detail-page/components/change-logical-operation-type-modal/ChangeLogicalOperationTypeModal.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Box, FormControl, Grid, InputLabel, MenuItem, Modal, Select, SelectChangeEvent } from '@mui/material'
+import { FormControl, Grid, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material'
 import { LogicalOperationNodeType } from '../editable-tree/EditableTree'
-import styles from './styles.module.scss'
+import MuiModalBase from '../../../../page-independent-components/mui-modal-base/MuiModalBase'
 
 interface ChangeLogicalOperationTypeModalProps {
   isOpen: boolean
@@ -9,47 +9,31 @@ interface ChangeLogicalOperationTypeModalProps {
   changeLogicalOperationType: (newOperationType: LogicalOperationNodeType) => void
 }
 
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  backgroundColor: '#ffffff',
-  border: '3px solid #000',
-  p: 2
-}
-
 const ChangeLogicalOperationTypeModal: React.FC<ChangeLogicalOperationTypeModalProps> = (props) => {
   const handleChange = (event: SelectChangeEvent<LogicalOperationNodeType>) => {
     props.changeLogicalOperationType(event.target.value as LogicalOperationNodeType)
     props.onCloseHandler()
   }
-
   return (
-    <div>
-      <Modal open={props.isOpen}>
-        <Box sx={style}>
-          <div className={styles.headerRow}>
-            <p>Change logical operation type</p>
-            <span onClick={props.onCloseHandler} className="material-symbols-outlined">
-              close
-            </span>
-          </div>
-          <Grid container spacing={2} alignItems="center">
-            <Grid item xs={8.8}>
-              <FormControl fullWidth>
-                <InputLabel id="select-field-label">Logical operation type</InputLabel>
-                <Select labelId="select-field-label" value="" label="Logical operation type" onChange={handleChange}>
-                  <MenuItem value={LogicalOperationNodeType.AND}>AND</MenuItem>
-                  <MenuItem value={LogicalOperationNodeType.OR}>OR</MenuItem>
-                  <MenuItem value={LogicalOperationNodeType.NOR}>NOR</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
+    <MuiModalBase
+      isOpen={props.isOpen}
+      onCloseHandler={props.onCloseHandler}
+      modalTitle="Change logical operation type"
+      content={
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs={8.8}>
+            <FormControl fullWidth>
+              <InputLabel id="select-field-label">Logical operation type</InputLabel>
+              <Select labelId="select-field-label" value="" label="Logical operation type" onChange={handleChange}>
+                <MenuItem value={LogicalOperationNodeType.AND}>AND</MenuItem>
+                <MenuItem value={LogicalOperationNodeType.OR}>OR</MenuItem>
+                <MenuItem value={LogicalOperationNodeType.NOR}>NOR</MenuItem>
+              </Select>
+            </FormControl>
           </Grid>
-        </Box>
-      </Modal>
-    </div>
+        </Grid>
+      }
+    ></MuiModalBase>
   )
 }
 
