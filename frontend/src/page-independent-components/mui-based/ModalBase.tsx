25d8ed4b3e1aa25c714faff4118392fd81b48a39
Michal Bureš <MichalBRS2020@gmail.com>
2024-05-18 01:44:15 +0200

    Getting rid of the undesired max-width property of MUI dialog(s).

--- frontend/src/page-independent-components/mui-based/ModalBase.tsx
+++ frontend/src/page-independent-components/mui-based/ModalBase.tsx
@@ -12,7 +12,15 @@ interface MuiModalBaseProps {
 
 const ModalBase: React.FC<MuiModalBaseProps> = (props) => {
   return (
-    <Dialog open={props.isOpen} onClose={props.onClose}>
+    <Dialog
+      open={props.isOpen}
+      onClose={props.onClose}
+      PaperProps={{
+        sx: {
+          maxWidth: 'none'
+        }
+      }}
+    >
       <DialogContent sx={{ width: `${props.vwPercentage ?? 20}vw` }}>
         <div className="mb-3 flex items-center justify-between gap-2.5">
           <p className="m-0 text-center text-2xl">{props.modalTitle}</p>
