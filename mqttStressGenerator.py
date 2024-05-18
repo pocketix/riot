19fe1675df4e5c32b7730fa63a1bfd9172e9dbd0
Michal Bureš <MichalBRS2020@gmail.com>
2024-05-18 17:39:58 +0200

    MQTT message stress test script parametrization: messages per second, SD instance mode(s): one, few, many and unlimited.

--- mqttStressGenerator.py
+++ mqttStressGenerator.py
@@ -2,11 +2,35 @@ import paho.mqtt.client as mqtt
 import time
 import json
 import random
+import argparse
+import enum
 
-messages_per_second = 100
+
+class SDInstanceMode(enum.Enum):
+    ONE = "one"
+    FEW = "few"
+    MANY = "many"
+    UNLIMITED = "unlimited"
+
+
+def sequential_number_generator():
+    number = 1
+    while True:
+        yield number
+        number += 1
+
+
+sng = sequential_number_generator()
 
 
 def generate_message_payload():
+    sd_instance_uid_suffix = {
+        SDInstanceMode.ONE: 1,
+        SDInstanceMode.FEW: random.randint(1, 3),
+        SDInstanceMode.MANY: random.randint(1, 100),
+        SDInstanceMode.UNLIMITED: next(sng)
+    }.get(sd_instance_mode)
+    sd_instance_uid = f"shelly30C6F787B4CCC-{sd_instance_uid_suffix}"
     return json.dumps({
         "ntf": {
             "msgId": "a210a169-5953-4fc2-8bdb-e595e18f0c90",
@@ -16,13 +40,13 @@ def generate_message_payload():
             "devs": [
                 {
                     "devType": "shelly1pro",
-                    "devUid": "shelly30C6F787B4CCC",
+                    "devUid": sd_instance_uid,
                     "devAttrs": {},
                     "devPars": {
                         "mac": "30C6F787B4CC",
                         "fw_id": "20220512-131622/0.10.2-beta1-gd0e2a8c",
                         "eco_mode": True,
-                        "deviceUid": "shelly30C6F787B4CCC",
+                        "deviceUid": sd_instance_uid,
                         "model_name": "shelly1pro",
                         "tst": 1680284232.0,
                         "relay_0_source": "WS_in",
@@ -32,7 +56,7 @@ def generate_message_payload():
                 }
             ]
         },
-        "topic": "IotLogimic/dev5/shelly/shelly1pro/shelly30C6F787B4CCC/john/ntf",
+        "topic": f"IotLogimic/dev5/shelly/shelly1pro/{sd_instance_uid}/john/ntf",
         "topicPrefix": "IotLogimic",
         "appId": "john",
         "dir": "ntf",
@@ -41,11 +65,17 @@ def generate_message_payload():
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='bp-bures-SfPDfSD MQTT stress test script')
+    parser.add_argument('--messages-per-second', type=int, default=10, help='Number of MQTT messages to send per second')
+    parser.add_argument('--sd-instance-mode', type=lambda s: SDInstanceMode[s.upper()], choices=list(SDInstanceMode), default=SDInstanceMode.ONE, help='SD instance mode: from how many different SD instances should the messages come')
+    args = parser.parse_args()
+    messages_per_second = args.messages_per_second
+    sd_instance_mode = args.sd_instance_mode
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
     client.username_pw_set("admin", "password")
     client.connect("host.docker.internal", 1883)
     client.loop_start()
-    print(f"Starting the stress test of bp-bures-SfPDfSD... {messages_per_second} MQTT messages per second")
+    print(f"Starting the stress test of bp-bures-SfPDfSD... {messages_per_second} MQTT message(s) per second")
     message_counter = 0
     try:
         while True:
@@ -60,4 +90,4 @@ if __name__ == '__main__':
     finally:
         client.loop_stop()
         client.disconnect()
-    print(f"Stress test of bp-bures-SfPDfSD completed... {message_counter} MQTT messages sent...")
+    print(f"Stress test of bp-bures-SfPDfSD completed... {message_counter} MQTT message(s) sent...")
