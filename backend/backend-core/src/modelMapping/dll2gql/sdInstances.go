4f2e3fe779d2ef6b43e790729256b765f9e1e383
thomson191919 <47994011+thomson191919@users.noreply.github.com>
2025-03-12 13:44:44 +0100

    ddl to gql mapping added

--- backend/backend-core/src/modelMapping/dll2gql/sdInstances.go
+++ backend/backend-core/src/modelMapping/dll2gql/sdInstances.go
@@ -7,10 +7,26 @@ import (
 
 func ToGraphQLModelSDInstance(sdInstance dllModel.SDInstance) graphQLModel.SDInstance {
 	return graphQLModel.SDInstance{
-		ID:              sdInstance.ID.GetPayload(),
-		UID:             sdInstance.UID,
-		ConfirmedByUser: sdInstance.ConfirmedByUser,
-		UserIdentifier:  sdInstance.UserIdentifier,
-		Type:            ToGraphQLModelSDType(sdInstance.SDType),
+		ID:                 sdInstance.ID.GetPayload(),
+		UID:                sdInstance.UID,
+		ConfirmedByUser:    sdInstance.ConfirmedByUser,
+		UserIdentifier:     sdInstance.UserIdentifier,
+		Type:               ToGraphQLModelSDType(sdInstance.SDType),
+		CommandInvocations: ToGraphQLModelSDCommandInvocations(sdInstance.CommandInvocations),
 	}
+}
+
+func ToGraphQLModelSDCommandInvocations(invocations []dllModel.SDCommandInvocation) []graphQLModel.SDCommandInvocation {
+	result := make([]graphQLModel.SDCommandInvocation, len(invocations))
+	for i, invocation := range invocations {
+		result[i] = graphQLModel.SDCommandInvocation{
+			ID:             invocation.ID,
+			InvocationTime: invocation.InvocationTime,
+			Payload:        invocation.Payload,
+			UserID:         invocation.UserID,
+			CommandID:      invocation.CommandID,
+			SdInstanceID:   invocation.SDInstanceID,
+		}
+	}
+	return result
 }
